605835fed1b0f11cd730b997b52aa81bbc47db30
hiuchida <hiro4uchida@gmail.com>
2016-09-11 15:41:58 +0900

    LoginUtilのJUnit作成

---
+++ src/ateam/test/util/HttpSessionMock.java
@@ -0,0 +1,124 @@
+package ateam.test.util;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
+
+import ateam.model.User;
+
+public class HttpSessionMock implements HttpSession {
+
+	private Map<String, Object> attrs = new HashMap<String, Object>();
+
+	public HttpSessionMock(User user) {
+		if (user != null) {
+			attrs.put("user", user);
+		}
+	}
+
+	@Override
+	public Object getAttribute(String arg0) {
+		return attrs.get(arg0);
+	}
+
+	@Override
+	public Enumeration<String> getAttributeNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getCreationTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getLastAccessedTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxInactiveInterval() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpSessionContext getSessionContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getValue(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getValueNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void invalidate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isNew() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void putValue(String arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeAttribute(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeValue(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAttribute(String arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setMaxInactiveInterval(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/ateam/test/util/LoginUtilTest.java
+++ src/ateam/test/util/LoginUtilTest.java
@@ -1,10 +1,36 @@
 package ateam.test.util;
 
+import ateam.model.User;
 import ateam.util.LoginUtil;
 import junit.framework.TestCase;
 
 public class LoginUtilTest extends TestCase {
-	public void testIsLogined() throws Exception {
+	public void testIsLogined1() throws Exception {
+		HttpServletRequestMock request = new HttpServletRequestMock(false, null);
+		HttpServletResponseMock response = new HttpServletResponseMock();
+		assertFalse(LoginUtil.isLogined(request, response));
+		assertEquals("LoginServlet?timeout=true", response.getRedirect());
+	}
+
+	public void testIsLogined2() throws Exception {
+		HttpServletRequestMock request = new HttpServletRequestMock(true, null);
+		HttpServletResponseMock response = new HttpServletResponseMock();
+		assertFalse(LoginUtil.isLogined(request, response));
+		assertEquals("LoginServlet?timeout=true", response.getRedirect());
+	}
+
+	public void testIsLogined3() throws Exception {
+		HttpServletRequestMock request = new HttpServletRequestMock(true, new User());
+		HttpServletResponseMock response = new HttpServletResponseMock();
+		assertTrue(LoginUtil.isLogined(request, response));
+		assertEquals(null, response.getRedirect());
+	}
+
+	public void testIsLogined4() throws Exception {
+		HttpServletRequestMock request = new HttpServletRequestMock(false, null);
+		HttpServletResponseMock response = new HttpServletResponseMock();
+		assertFalse(LoginUtil.isLogined(request, null));
+		assertFalse(LoginUtil.isLogined(null, response));
 		assertFalse(LoginUtil.isLogined(null, null));
 	}
 
